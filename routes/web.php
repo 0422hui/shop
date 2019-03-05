33e8c9376264f8454fec45654cc10b2b8fe82d03
0422hui <980399260@qq.com>
2019-03-05 08:27:09 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Encore\Admin\Auth\Database\Role;
 
-
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 //Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
