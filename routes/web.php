2f871d90d081fb979c5a70a1ea99e5cab3fb8115
0422hui <980399260@qq.com>
2019-03-06 04:24:22 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Encore\Admin\Auth\Database\Role;
-
 Route::get('products', 'ProductsController@index')->name('products.index');
 
 //Route::get('/', 'PagesController@root')->name('root');
@@ -22,5 +20,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    Route::post('cart', 'CartController@add')->name('cart.add');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
